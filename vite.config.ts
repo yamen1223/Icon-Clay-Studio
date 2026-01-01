0109cbadfcb9af0356c512d645e74e3c6e2c5b77
Matthew Wesney <157447210+dovvnloading@users.noreply.github.com>
2026-01-01 13:12:34 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Base URL required for GitHub Pages deployment
+      base: '/Icon-Clay-Studio/',
       server: {
         port: 3000,
         host: '0.0.0.0',
